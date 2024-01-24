8938a4376fcfaee2a888196ddb122457683a7606
Jinyong Kim <wlsdyd4@naver.com>
2024-01-24 17:42:32 +0900

    [BE] Feat : checkId

--- backend/src/main/java/com/ssafy/backend/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/backend/member/service/MemberService.java
@@ -1,13 +1,14 @@
 package com.ssafy.backend.member.service;
 
 import com.ssafy.backend.member.domain.Member;
+import com.ssafy.backend.member.dto.request.RequestCheckIdDto;
 import com.ssafy.backend.member.dto.request.RequestLocalLoginDto;
 
 import java.util.Map;
 
 public interface MemberService {
 
-    public Member findByMemberId(String memberId);
+    public boolean checkId(RequestCheckIdDto requestCheckIdDto);
 
     public Map<String, Object> localLogin(RequestLocalLoginDto loginDto);
 
