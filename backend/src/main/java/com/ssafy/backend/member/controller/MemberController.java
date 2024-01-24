c321903f29142cb8963a45ad41170439d3ead290
minjung <mjmj9999@sookmyung.ac.kr>
2024-01-24 09:44:22 +0900

    [BE] Feat : Local Login

--- backend/src/main/java/com/ssafy/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/backend/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.member.controller;
 
 import com.ssafy.backend.member.domain.Member;
+import com.ssafy.backend.member.dto.MemberDto;
 import com.ssafy.backend.member.dto.RequestMemberCheckIdDto;
 import com.ssafy.backend.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -60,5 +61,17 @@ public class MemberController {
         return new ResponseEntity<>(resultMap, status);
     }
 
+    @PostMapping("/local-login")
+    public ResponseEntity<?> localLogin(@RequestBody MemberDto memberDto){
+        Map<String, Object> resultMap = new HashMap<>();
+
+        if(memberService.localLogin(memberDto)){
+            resultMap.put("message", "OK");
+            return ResponseEntity.status(HttpStatus.OK).body(resultMap);
+        }else{
+            resultMap.put("message", "아이디 혹은 비밀번호를 확인해주세요.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
+        }
+    }
 
 }
--- backend/src/main/java/com/ssafy/backend/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/member/service/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.member.service;
 
 import com.ssafy.backend.member.domain.Member;
+import com.ssafy.backend.member.dto.MemberDto;
 import com.ssafy.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,20 @@ public class MemberServiceImpl implements MemberService {
         return memberRepository.findByMemberId(memberId);
     }
 
+    @Override
+    public boolean localLogin(MemberDto memberDto) {
+        Member member = memberRepository.findByMemberId(memberDto.getMemberId());
 
+        if(member == null){ // 아이디에 해당하는 회원 없을때
+            return false;
+        }else{
+            if(member.isDeleted()){ // 탈퇴한 회원일때
+                return false;
+            }
+            if(!memberDto.getPassword().equals(member.getPassword())){ // 비번 틀렸을때
+                return false;
+            }
+        }
+        return true;
+    }
 }
