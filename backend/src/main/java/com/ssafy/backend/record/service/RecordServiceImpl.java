1c7e01c577ee41373d9edce95d14d945db6f918f
New Name Value <mjmj9999@naver.com>
2024-01-25 17:46:25 +0900

    [BE] Feat : 산책 기록 등록 구현 중

---
+++ backend/src/main/java/com/ssafy/backend/record/service/RecordServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ssafy.backend.record.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecordServiceImpl implements RecordService{
+}
