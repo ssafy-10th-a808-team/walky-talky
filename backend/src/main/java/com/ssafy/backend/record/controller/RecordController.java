f9c3e3b2b904c10802fe2ffe6ce6137bc3a7f2fd
minjung <mjmj9999@sookmyung.ac.kr>
2024-01-26 16:59:08 +0900

    [BE] Feat : 한줄평 등록

--- backend/src/main/java/com/ssafy/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/ssafy/backend/record/controller/RecordController.java
@@ -1,5 +1,6 @@
 package com.ssafy.backend.record.controller;
 
+import com.ssafy.backend.record.dto.request.RequestRegistCommentDto;
 import com.ssafy.backend.record.dto.request.RequestRegistRecordDto;
 import com.ssafy.backend.record.service.RecordService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -51,7 +52,7 @@ public class RecordController {
         if (msg == null) {
             Long memberSeq = (Long) request.getAttribute("seq");
 
-            if(!recordService.registRecord(memberSeq, requestRegistRecordDto)){
+            if (!recordService.registRecord(memberSeq, requestRegistRecordDto)) {
                 resultMap.put("message", "산책 기록 등록에 실패하였습니다.");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
             }
@@ -62,8 +63,27 @@ public class RecordController {
             resultMap.put("message", msg);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(resultMap);
         }
+    }
 
+    @PostMapping("/regist-comment")
+    public ResponseEntity<?> registComment(HttpServletRequest request, @RequestBody RequestRegistCommentDto requestRegistCommentDto) {
+        Map<String, Object> resultMap = new HashMap<>();
 
+        String msg = (String) request.getAttribute("message");
+        if (msg == null) {
+            Long memberSeq = (Long) request.getAttribute("seq");
+
+            if(!recordService.registComment(memberSeq, requestRegistCommentDto)){
+                resultMap.put("message", "산책 기록 등록에 실패하였습니다.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
+            }
+
+            resultMap.put("message", "OK");
+            return ResponseEntity.status(HttpStatus.OK).body(resultMap);
+        } else {
+            resultMap.put("message", msg);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(resultMap);
+        }
     }
 
 }
