e1c96f74874bb0cd0f2f7eb11416a13a0dc1e5e6
KimJinyong <wlsdyd4@naver.com>
2024-01-28 01:50:36 +0900

    [BE] Feat : club create

--- backend/src/main/java/com/ssafy/backend/club/service/ClubService.java
+++ backend/src/main/java/com/ssafy/backend/club/service/ClubService.java
@@ -1,7 +1,12 @@
 package com.ssafy.backend.club.service;
 
+import com.ssafy.backend.club.domain.Club;
 import com.ssafy.backend.club.dto.request.RequestCheckNameDto;
+import com.ssafy.backend.club.dto.request.RequestClubCreateDto;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface ClubService {
     boolean checkName(RequestCheckNameDto requestCheckNameDto);
+
+    Club clubCreate(RequestClubCreateDto requestClubCreateDto, HttpServletRequest httpServletRequest);
 }
