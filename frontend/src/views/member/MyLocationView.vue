1d6d181610308a5b42fac2d0b7d645738456eda4
imdeokey <imdeokey@gmail.com>
2024-01-28 23:00:41 +0900

    [FE] Fix : 카카오맵 비동기 문제로 안되던 것 수정

--- frontend/src/views/member/MyLocationView.vue
+++ frontend/src/views/member/MyLocationView.vue
@@ -15,16 +15,21 @@
     onMounted(() => {
         if (window.kakao && window.kakao.maps) {
             initMap();
-            searchDetailAddrFromCoords(lat, lon, addrCallback)
+            // searchDetailAddrFromCoords(lat, lon, addrCallback)
         } else {
             
             const script = document.createElement('script');
             // eslint 사용 시  kakao 변수가 선언되지 않았다고 오류가 나기 때문에 아래줄 추가
             /* global kakao */
-            script.onload = () => kakao.maps.load(initMap);
+            script.onload = () => {
+                console.log('카카오맵 api script loaded')
+                kakao.maps.load(initMap);
+            }
             script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}&libraries=services&autoload=false`;
             //autoload=false를 통해 로딩이 끝나는 시점에 콜백을 통해 객체에 접근 
             document.head.appendChild(script);
+
+
             
         }
 
@@ -34,18 +39,22 @@
                 lon = position.coords.longitude // 경도
                 // geolocation 가능한 경우 내 위치
                 // 크롬 브라우저는 https 환경에서만 geolocation이 지원된다고 하네요 local도 되긴 했음
+                console.log("내 좌표를 가져왔습니다")
                 
             })
         } else {
             lat = 37.5014
             lon = 127.0395 
             // geolocation 불가능하면 위치를 멀티캠퍼스로
+            console.log("멀티캠퍼스 좌표를 가져왔습니다")
         }
  
     });
 
 
     const initMap = () => {
+        console.log("initMap 적용")
+
         const marker = new kakao.maps.Marker({
             position: new kakao.maps.LatLng(lat, lon)
         })
@@ -55,25 +64,32 @@
             level: 5,
         }
         map = new kakao.maps.Map(container, options)
+
         marker.setMap(map)
 
+        const geocoder = new kakao.maps.services.Geocoder()
+        geocoder.coord2Address(lat, lon, addrCallback)
     }
+ 
+    
 
-  
-    const searchDetailAddrFromCoords = (lat, lon, callback) => {
-        // 좌표로 법정동 상세 주소 정보를 요청합니다
-        const geocoder = kakao.maps.services.Geocoder()
-        geocoder.coord2Address(lat, lon, callback);
-    }
+    // const searchDetailAddrFromCoords = (lat, lon, callback) => {
+    //     console.log("좌표 가져오는 코드")
+    //         this.geocoder.coord2Address(lat, lon, callback);
+    
+    //     // 좌표로 법정동 상세 주소 정보를 요청합니다
+    // }
 
     const addrCallback = (result, status) => {
         // 법정동 상세 주소를 가져올 때 콜백 함수를 선언한 것입니다
-        if (status === kakao.services.Status.OK) {
+        if (status === kakao.maps.services.Status.OK) {
             console.log(result)
         } else {
-            console.error()
+            console.error("Failed to get address info")
+            console.log(kakao.maps.services.Status)
         }
     }
+
     
 
 
