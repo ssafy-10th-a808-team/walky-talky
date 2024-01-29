cb97b079fd22f26b6c8a067ba09fa62af95fb5e5
New Name Value <mjmj9999@naver.com>
2024-01-29 12:56:49 +0900

    [BE] Fix : 사진 수정 코드 수정

--- backend/src/main/java/com/ssafy/backend/record/repository/RecordDetailRepository.java
+++ backend/src/main/java/com/ssafy/backend/record/repository/RecordDetailRepository.java
@@ -2,9 +2,6 @@ package com.ssafy.backend.record.repository;
 
 import com.ssafy.backend.record.domain.RecordDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,9 +10,5 @@ import java.util.List;
 public interface RecordDetailRepository extends JpaRepository<RecordDetail, Long> {
 
     List<RecordDetail> findAllByRecordSeq(Long recordSeq);
-
-    @Modifying
-    @Query("UPDATE RecordDetail rd SET rd.url = :url WHERE rd.seq = :recordDetailSeq")
-    void updateUrl(@Param("recordDetailSeq") Long recordDetailSeq, @Param("url") String url);
 
 }
