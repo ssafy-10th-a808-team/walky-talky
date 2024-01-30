6ea199dfd912821ba4012a67f261e7411f0d2f4e
imdeokey <imdeokey@gmail.com>
2024-01-30 13:01:00 +0900

    [FE] Design : Bootstrap 템플릿 적용

--- frontend/src/components/common/TheFooter.vue
+++ frontend/src/components/common/TheFooter.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <footer>
-      <nav class="nav"><a href="https://edu.ssafy.com/">SSAFY</a>&nbsp;&nbsp;</nav>
-      <h3>나는 푸터</h3>
-    </footer>
-  </div>
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>WalkyTalky</h3>
+      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>Green</span></strong>. All Rights Reserved
+      </div>
+      
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
 </template>
 
 <script setup></script>
 
 <style scoped>
-h3 {
-  background-color: rgb(244, 106, 210);
-}
 </style>
