dfbc6e3ad5e2373fac1b70740361cca449569208
kimyeonhwa <itidnsld@naver.com>
2024-01-30 15:13:09 +0900

    [FE] Feat : button and stopwatch on map

---
+++ frontend/src/components/walk/StopWatch.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      {{ count }}
+      <button @click="play">P</button>
+      <button @click="stop">S</button>
+      <button @click="reset">R</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+let intervalId
+const play = () => {
+  intervalId = setInterval(() => {
+    count.value++
+  }, 1000)
+}
+const stop = () => clearInterval(intervalId)
+const reset = () => {
+  stop()
+  count.value = 0
+}
+</script>
+
+<style scoped>
+div {
+  margin: 10px;
+}
+</style>
