15fe3259180764a001813fd77724fa04f5982fd4
minjung <mjmj9999@sookmyung.ac.kr>
2024-01-30 12:19:11 +0900

    [BE] Feat : 기록 공유 게시판 목록 완료

--- backend/src/main/java/com/ssafy/backend/record/repository/RecordDetailRepository.java
+++ backend/src/main/java/com/ssafy/backend/record/repository/RecordDetailRepository.java
@@ -3,7 +3,6 @@ package com.ssafy.backend.record.repository;
 import com.ssafy.backend.record.domain.RecordDetail;
 import com.ssafy.backend.record.dto.PointMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,8 +13,6 @@ public interface RecordDetailRepository extends JpaRepository<RecordDetail, Long
 
     List<RecordDetail> findAllByRecordSeq(Long recordSeq);
 
-    @Query("SELECT r.latitude, r.longitude FROM RecordDetail r " +
-            "WHERE r.recordSeq = :recordSeq AND r.url IS NULL AND r.pointComment IS NULL")
     List<PointMapping> findLatitudeAndLongitudeByRecordSeq(@Param("recordSeq") Long recordSeq);
 
 }
