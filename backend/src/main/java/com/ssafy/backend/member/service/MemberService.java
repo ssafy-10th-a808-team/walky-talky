ab834c1dd237f9e7c740d7947428d3bd2350bd8d
New Name Value <mjmj9999@naver.com>
2024-02-01 09:49:16 +0900

    [BE] Feat : Login null check

--- backend/src/main/java/com/ssafy/backend/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/backend/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.ssafy.backend.member.service;
 
+import com.ssafy.backend.global.error.WTException;
 import com.ssafy.backend.member.dto.request.RequestCheckIdDto;
 import com.ssafy.backend.member.dto.request.RequestCheckNicknameDto;
 import com.ssafy.backend.member.dto.request.RequestLocalLoginDto;
@@ -13,7 +14,7 @@ public interface MemberService {
 
     boolean checkId(RequestCheckIdDto requestCheckIdDto);
 
-    public Map<String, Object> localLogin(RequestLocalLoginDto loginDto) throws NoSuchAlgorithmException;
+    public Map<String, Object> localLogin(RequestLocalLoginDto loginDto) throws WTException;
 
     public void logout(Long seq);
 
