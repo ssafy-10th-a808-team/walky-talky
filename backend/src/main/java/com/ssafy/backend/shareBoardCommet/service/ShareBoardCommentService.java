2e332bbdd6d6c65ca578489c7d1fa3a4b9dc817d
minjung <mjmj9999@sookmyung.ac.kr>
2024-02-02 08:43:42 +0900

    [BE] Fix : filter - controller 수정

--- backend/src/main/java/com/ssafy/backend/shareBoardCommet/service/ShareBoardCommentService.java
+++ backend/src/main/java/com/ssafy/backend/shareBoardCommet/service/ShareBoardCommentService.java
@@ -2,7 +2,7 @@ package com.ssafy.backend.shareBoardCommet.service;
 
 import com.ssafy.backend.global.error.WTException;
 
-public interface CommentService {
+public interface ShareBoardCommentService {
 
     void commentWrite(Long seq, Long memberSeq, String content) throws WTException;
 
