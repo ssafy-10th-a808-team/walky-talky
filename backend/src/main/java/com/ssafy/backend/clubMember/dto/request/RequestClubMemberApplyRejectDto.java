86402ffc0190e6326e44467057a9ab7f27732967
KimJinyong <wlsdyd4@naver.com>
2024-02-01 23:26:51 +0900

    [BE] Feat : member

--- backend/src/main/java/com/ssafy/backend/clubMember/dto/request/RequestClubMemberApplyRejectDto.java
+++ backend/src/main/java/com/ssafy/backend/clubMember/dto/request/RequestClubMemberApplyRejectDto.java
@@ -1,10 +1,8 @@
 package com.ssafy.backend.clubMember.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
