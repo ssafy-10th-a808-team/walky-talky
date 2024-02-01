6b1f2a010719da72196a30ba58c9451600521530
wooseobee <dntjq0807@naver.com>
2024-02-01 13:10:57 +0900

    [BE] Feat : 채팅내역 10개씩 불러오기 구현

--- backend/src/main/java/com/ssafy/backend/chat/service/ChatMessageServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/chat/service/ChatMessageServiceImpl.java
@@ -26,16 +26,19 @@ public class ChatMessageServiceImpl implements ChatMessageService {
     }
 
     @Override
-    public List<ChatMessageDto> loadMessage(Long chatSeq) {
+    public List<ChatMessageDto> loadMessage(Long chatSeq, Long offset) {
         List<ChatMessageDto> messageList = new ArrayList<>();
-        List<ChatMessageDto> redisMessageList = redisTemplateMessage.opsForList().range(CHAT_ROOM + "-" + chatSeq + ":", 0, 99);
+        List<ChatMessageDto> redisMessageList = redisTemplateMessage.opsForList().range(CHAT_ROOM + "-" + chatSeq + ":", offset * 10, (offset * 10) + 9);
         if (redisMessageList == null || redisMessageList.isEmpty()) {
             List<ChatMessage> dbMessageList = chatMessageRepository.findByChatSeqOrderByCreatedAt(chatSeq);
+            int cnt = 0;
             for (ChatMessage chatMessage : dbMessageList) {
+                if (cnt == 10) break;
                 redisTemplateMessage.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDto.class));
                 ChatMessageDto requestChatMessageDto = new ChatMessageDto(chatSeq, chatMessage.getSender(), chatMessage.getContent(), chatMessage.getCreatedAt(), chatMessage.getType());
                 redisTemplateMessage.opsForList().rightPush(CHAT_ROOM + "-" + chatSeq + ":", requestChatMessageDto);
                 messageList.add(requestChatMessageDto);
+                cnt++;
             }
         } else {
             messageList.addAll(redisMessageList);
