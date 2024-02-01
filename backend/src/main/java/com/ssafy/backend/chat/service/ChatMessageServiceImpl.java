3b28cafab00d42631c0683f56416dc44714d9ccb
wooseobee <dntjq0807@naver.com>
2024-02-01 14:17:39 +0900

    [BE] Feat : 채팅메시지 pagination 구현

--- backend/src/main/java/com/ssafy/backend/chat/service/ChatMessageServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/chat/service/ChatMessageServiceImpl.java
@@ -3,12 +3,16 @@ package com.ssafy.backend.chat.service;
 import com.ssafy.backend.chat.domain.Chat;
 import com.ssafy.backend.chat.domain.ChatMessage;
 import com.ssafy.backend.chat.domain.dto.ChatMessageDto;
+import com.ssafy.backend.chat.domain.dto.MessageType;
 import com.ssafy.backend.chat.repository.ChatMessageRepository;
 import com.ssafy.backend.chat.repository.ChatRepository;
 import com.ssafy.backend.global.error.WTException;
+import com.ssafy.backend.member.domain.Member;
+import com.ssafy.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,10 +22,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatMessageServiceImpl implements ChatMessageService {
 
-    private final RedisTemplate<String, ChatMessageDto> redisTemplateMessage;
+//    private final RedisTemplate<String, ChatMessageDto> redisTemplateMessage;
+    private final MemberRepository memberRepository;
     private final ChatRepository chatRepository;
     private final ChatMessageRepository chatMessageRepository;
-    private static final String CHAT_ROOM = "chat";
+//    private static final String CHAT_ROOM = "chat";
 
     @Override
     public void saveMessage(ChatMessageDto chatMessageDto) {
@@ -44,22 +49,33 @@ public class ChatMessageServiceImpl implements ChatMessageService {
     }
 
     @Override
-    public List<ChatMessageDto> loadMessage(Long chatSeq, Long offset) {
+    public List<ChatMessageDto> loadMessage(Long memberSeq, Long chatSeq, int offset) {
         List<ChatMessageDto> messageList = new ArrayList<>();
-        List<ChatMessageDto> redisMessageList = redisTemplateMessage.opsForList().range(CHAT_ROOM + "-" + chatSeq + ":", offset * 10, (offset * 10) + 9);
-        if (redisMessageList == null || redisMessageList.isEmpty()) {
-            List<ChatMessage> dbMessageList = chatMessageRepository.findByChatSeqOrderByCreatedAt(chatSeq);
-            int cnt = 0;
-            for (ChatMessage chatMessage : dbMessageList) {
-                if (cnt == 10) break;
-                redisTemplateMessage.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDto.class));
-                ChatMessageDto requestChatMessageDto = new ChatMessageDto(chatSeq, chatMessage.getSender(), chatMessage.getContent(), chatMessage.getCreatedAt(), chatMessage.getType());
-                redisTemplateMessage.opsForList().rightPush(CHAT_ROOM + "-" + chatSeq + ":", requestChatMessageDto);
-                messageList.add(requestChatMessageDto);
-                cnt++;
+//        캐싱 적용
+//        List<ChatMessageDto> redisMessageList = redisTemplateMessage.opsForList().range(CHAT_ROOM + "-" + chatSeq + ":", offset * 10, (offset * 10) + 9);
+//        if (redisMessageList == null || redisMessageList.isEmpty()) {
+//            List<ChatMessage> dbMessageList = chatMessageRepository.findByChatSeqOrderByCreatedAt(chatSeq);
+//            int cnt = 0;
+//            for (ChatMessage chatMessage : dbMessageList) {
+//                if (cnt == 10) break;
+//                redisTemplateMessage.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDto.class));
+//                ChatMessageDto requestChatMessageDto = new ChatMessageDto(chatSeq, chatMessage.getSender(), chatMessage.getContent(), chatMessage.getCreatedAt(), chatMessage.getType());
+//                redisTemplateMessage.opsForList().rightPush(CHAT_ROOM + "-" + chatSeq + ":", requestChatMessageDto);
+//                messageList.add(requestChatMessageDto);
+//                cnt++;
+//            }
+//        } else {
+//            messageList.addAll(redisMessageList);
+//        }
+        Pageable pageable = PageRequest.of(offset, 10);
+        List<ChatMessage> dbMessageList = chatMessageRepository.findByChatSeqOrderByCreatedAt(chatSeq, pageable);
+        Member findMember = memberRepository.findById(memberSeq).orElse(null);
+        for (ChatMessage chatMessage : dbMessageList) {
+            if (chatMessage.getSender().equals(findMember.getNickname()) && MessageType.JOIN.equals(chatMessage.getType())) {
+                break;
             }
-        } else {
-            messageList.addAll(redisMessageList);
+            ChatMessageDto requestChatMessageDto = new ChatMessageDto(chatSeq, chatMessage.getSender(), chatMessage.getContent(), chatMessage.getCreatedAt(), chatMessage.getType());
+            messageList.add(requestChatMessageDto);
         }
         return messageList;
     }
