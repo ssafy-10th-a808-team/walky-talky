9512121e592ec4ac39715aaa61d0d5a96671e31f
imdeokey <imdeokey@gmail.com>
2024-02-03 23:34:34 +0900

    [FE] Add : 로그인, 로그아웃마다 네비게이션 바 바뀌는 것으로 수정

--- frontend/src/components/common/TheFooter.vue
+++ frontend/src/components/common/TheFooter.vue
@@ -3,9 +3,9 @@
   <footer id="footer">
     <div class="container">
       <h3>WalkyTalky</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <p>산책해듀오</p>
       <div class="copyright">
-        &copy; Copyright <strong><span>Green</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>WalkyTalky</span></strong>. All Rights Reserved
       </div>
       
     </div>
--- frontend/src/components/common/TheHeaderNav.vue
+++ frontend/src/components/common/TheHeaderNav.vue
@@ -19,10 +19,10 @@
           <li><a class="nav-link scrollto" href="#services">산책공유</a></li>
           <!-- <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
           <li><a class="nav-link scrollto" href="#team">Team</a></li> -->
-          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"  v-if="memberstore.isLogin"><a href="#"><span>{{ memberstore.nickname }}</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+              <li><a href="#">마이페이지</a></li>
+              <!-- <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
                   <li><a href="#">Deep Drop Down 1</a></li>
                   <li><a href="#">Deep Drop Down 2</a></li>
@@ -30,18 +30,15 @@
                   <li><a href="#">Deep Drop Down 4</a></li>
                   <li><a href="#">Deep Drop Down 5</a></li>
                 </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              </li> -->
+              <li><RouterLink :to="{ name: 'Logout'}"><a href="#">로그아웃</a></RouterLink></li>
+              <!-- <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li> -->
             </ul>
-          </li> -->
+          </li>
           <!-- <li><a class="nav-link scrollto" href="#contact">Contact</a></li> -->
           <li v-if="!memberstore.isLogin">
             <RouterLink :to="{ name: 'Login' }" class="getstarted scrollto">로그인</RouterLink>
-          </li>
-          <li v-if="memberstore.isLogin">
-            <RouterLink :to="{ name: 'Logout' }" class="getstarted scrollto">로그아웃</RouterLink>
           </li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
@@ -55,6 +52,11 @@
 <script setup>
 import { useMemberStore } from '@/stores/member';
 const memberstore = useMemberStore()
+
+
+
+
+
 
 </script>
 
