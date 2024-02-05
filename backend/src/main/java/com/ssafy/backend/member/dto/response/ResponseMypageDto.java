452d64f389888699a93e77fc19892787b84afa72
minjung <mjmj9999@sookmyung.ac.kr>
2024-02-05 17:13:32 +0900

    [BE] Refactor : toString 삭제

--- backend/src/main/java/com/ssafy/backend/member/dto/response/ResponseMypageDto.java
+++ backend/src/main/java/com/ssafy/backend/member/dto/response/ResponseMypageDto.java
@@ -16,18 +16,4 @@ public class ResponseMypageDto {
     String regionCd;
     String introduce;
     String profileImage;
-
-    @Override
-    public String toString() {
-        return "ResponseMypageDto{" +
-                "nickname='" + nickname + '\'' +
-                ", birth='" + birth + '\'' +
-                ", gender='" + gender + '\'' +
-                ", memberId='" + memberId + '\'' +
-                ", address='" + address + '\'' +
-                ", regionCd='" + regionCd + '\'' +
-                ", introduce='" + introduce + '\'' +
-                ", profileImage='" + profileImage + '\'' +
-                '}';
-    }
 }
