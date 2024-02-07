e4eb95d9f74bcf9b451185f82569d55346ca0c3f
Jinyong Kim <wlsdyd4@naver.com>
2024-02-07 16:51:14 +0900

    [FE] Fix: headerNav

--- frontend/src/components/common/TheHeaderNav.vue
+++ frontend/src/components/common/TheHeaderNav.vue
@@ -29,7 +29,14 @@
             <RouterLink :to="{ name: 'club-chat', params: { seq: 8 } }" class="nav-link scrollto">채팅테스트</RouterLink>
           </li>
           <li class="dropdown" v-if="memberstore.isLogin">
-            <a href="#"><span>hi, {{ nickname }}</span> <i class="bi bi-chevron-down"></i></a>
+            <a href="#" class="profile-link">
+              <!-- 프로필 이미지와 닉네임을 감싸는 flex 컨테이너 -->
+              <div class="profile-container">
+                <img :src="url" class="profile-image" alt="Profile">
+                <span>{{ nickname }}</span>
+              </div>
+              <i class="bi bi-chevron-down"></i>
+            </a>
             <ul>
               <li @click="closeNav">
                 <RouterLink :to="{ name: 'Mypage' }"><a href="#">마이페이지</a></RouterLink>
@@ -66,8 +73,13 @@
 <script setup>
 import { handleError, ref } from 'vue'
 import { useMemberStore } from '@/stores/member'
+import { useCounterStore } from '@/stores/counter';
+
 const memberstore = useMemberStore()
-const nickname = ref(memberstore.nickname)
+const counterstore = useCounterStore()
+
+const nickname = counterstore.getCookie('nickname')
+const url = counterstore.getCookie('profileImage')
 
 // const closeNav = () => {
 //   const navbar = document.querySelector('#navbar')
@@ -86,4 +98,17 @@ const closeNav = () => {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.profile-image {
+  width: 40px;
+  /* 이미지 크기 설정 */
+  height: 40px;
+  /* 이미지 크기 설정 */
+  border-radius: 50%;
+  /* 원형으로 만들기 */
+  object-fit: cover;
+  /* 이미지 비율을 유지하면서 요소에 맞추기 */
+  margin-right: 10px;
+  /* 이미지와 닉네임 사이의 여백 설정 */
+}
+</style>
