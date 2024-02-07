a6f4dfc4ce17eac4b5ded4bd41d5810ed8c04082
minjung <mjmj9999@sookmyung.ac.kr>
2024-02-07 15:52:16 +0900

    [FE] Feat : 글 삭제, 댓글 변경 실시간 반영

--- frontend/src/views/shareboard/ShareBoardView.vue
+++ frontend/src/views/shareboard/ShareBoardView.vue
@@ -39,6 +39,7 @@ const shareBoardWrite = () => {
   <button class="share-button" @click="shareBoardWrite">내 산책 공유하기</button>
   <div v-for="(content, key) in contents" :key="content.shareBoardSeq">
     <shareBoardListItem
+      class="share-board-item"
       :content="content"
       :record="records[key]"
       :like="likes[key]"
@@ -52,5 +53,13 @@ const shareBoardWrite = () => {
 .share-button {
   margin-left: auto;
   display: flex;
+}
+
+.share-board-item {
+  box-sizing: border-box; /* 요소의 padding과 border를 너비에 포함시킴 */
+  margin: 20px; /* 각 요소 사이의 간격 설정 */
+  padding: 10px;
+  border: 1px solid #ccc; /* 테두리 스타일 및 색상 설정 */
+  border-radius: 5px; /* 테두리의 둥근 정도 설정 */
 }
 </style>
