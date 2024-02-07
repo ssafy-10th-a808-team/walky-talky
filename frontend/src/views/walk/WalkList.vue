1f95a50ac8313581a070d18ada2ce400bed7575b
myeon0109 <124342362+myeon0109@users.noreply.github.com>
2024-02-08 04:08:56 +0900

    [FE] Add : add functions

--- frontend/src/views/walk/WalkList.vue
+++ frontend/src/views/walk/WalkList.vue
@@ -1,12 +1,58 @@
 <template>
   <div>
-    <WalkHeaderNav />
-    <h1>내코스</h1>
+    <h5>내가 산책했던 코스 보기</h5>
+  </div>
+  <button class="share-button" @click="shareBoardWrite">내 산책 공유하기</button>
+  <div class="record-list-container">
+    <div
+      v-for="record in records"
+      :key="record.recordSeq"
+      :class="{ 'record-list': true, selected: selectedRecord === record.recordSeq }"
+      @click="selectRecord(record.recordSeq)"
+    >
+      <shareBoardRecord
+        :duration="record.duration"
+        :distance="record.distance"
+        :points="record.points"
+        :title="record.title"
+        :seq="record.recordSeq"
+        :movable="false"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+import { useShareBoardStore } from '@/stores/shareBoard'
 import WalkHeaderNav from '@/components/common/WalkHeaderNav.vue'
+import shareBoardRecord from '@/components/shareBoard/shareBoardRecord.vue'
+
+const shareBoardStore = useShareBoardStore()
+
+const records = ref([])
+onMounted(async () => {
+  await shareBoardStore.getMyRecord()
+  records.value = shareBoardStore.myRecords
+})
+
+const selectedRecord = ref(null)
+
+const selectRecord = (seq) => {
+  if (selectedRecord.value === seq) {
+    selectedRecord.value = null
+  } else {
+    selectedRecord.value = seq
+  }
+}
+const shareBoardWrite = () => {
+  router.push({ name: 'share-board-write' })
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.share-button {
+  margin-left: auto;
+  display: flex;
+}
+</style>
