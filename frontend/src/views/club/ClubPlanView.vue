4f8095902dd401ea3c95765253d7a1e5b995cd51
KimJinyong <wlsdyd4@naver.com>
2024-02-09 15:50:17 +0900

    [FE] Feat : plan

--- frontend/src/views/club/ClubPlanView.vue
+++ frontend/src/views/club/ClubPlanView.vue
@@ -1,46 +1,47 @@
 <template>
-    <ClubDetailHeaderNav />
-    {{ seq }}
-    {{ clubstore.clubDetail }}
-    {{ clubstore.clubSeq }}
+  <ClubDetailHeaderNav />
+  {{ clubstore.planList }}
+  <!-- <calendarComponent :events="clubstore.planList" @selectDate="handleDateSelect" /> -->
+  <!-- <div v-if="selectedDate">
+    <eventList :events="eventsForSelectedDate" @selectEvent="handleEventSelect" />
+  </div> -->
+  <button @click="createNewEvent">일정 생성</button>
 </template>
-  
+
 <script setup>
 import ClubDetailHeaderNav from '@/components/common/ClubDetailHeaderNav.vue'
 import { onMounted, ref } from 'vue'
 import { useClubStore } from '@/stores/club'
-import MemberList from '@/components/member/MemberListView.vue'
-import axios from 'axios';
-import { useCounterStore } from '@/stores/counter'
+import { useRouter } from 'vue-router'
+import calendarComponent from '@/views/club/CalendarComponent.vue'
+import eventList from '@/views/club/EventListComponent.vue'
 
 const clubstore = useClubStore()
+const selectedDate = ref(null)
+const eventsForSelectedDate = ref([])
+const router = useRouter()
 
 const { seq } = defineProps({
-    seq: String
+  seq: String
 })
 
-</script>
-  
-<style scoped>
-.circular {
-    width: 200px;
-    /* 고정 가로 크기 */
-    height: 200px;
-    /* 고정 세로 크기 */
-    border-radius: 50%;
-    /* 원 모양으로 만듦 */
-    overflow: hidden;
-    display: flex;
-    align-items: center;
-    /* 이미지를 중앙에 정렬 */
-    justify-content: center;
-}
+onMounted(async function () {
+  await clubstore.getPlanList()
+})
 
-.circular img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    /* 이미지 비율 유지하며 컨테이너 채움 */
-    border-radius: 50%;
+// const handleDateSelect = (date) => {
+//   selectedDate.value = date
+//   eventsForSelectedDate.value = clubstore.planList.filter((event) => event.date === date)
+// }
+
+// const handleEventSelect = (eventId) => {
+//   // 이벤트 ID를 사용하여 PlanDetail 페이지로 네비게이션 합니다.
+// }
+
+const createNewEvent = function () {
+  // 일정 생성 로직을 여기에 추가합니다.
+  router.push({ name: 'club-plan-regist', params: { seq: clubstore.clubSeq } })
 }
-</style>
+</script>
+
+<style scoped></style>
