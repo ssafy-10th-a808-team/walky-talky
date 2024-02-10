6ce01a12518893a70cd9bcd763e5b12b097ce928
Wooseobee <dntjq0807@naver.com>
2024-02-10 18:07:10 +0900

    [BE] Feat : 소모임 가입, 탈퇴 메시지 전송 구현 / 가입 이전의 채팅 내역 조회 제한 구현

--- backend/src/main/java/com/ssafy/backend/chat/domain/dto/ChatMessageDto.java
+++ backend/src/main/java/com/ssafy/backend/chat/domain/dto/ChatMessageDto.java
@@ -1,15 +1,13 @@
 package com.ssafy.backend.chat.domain.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ChatMessageDto {
     private Long clubSeq;
     private String sender;
