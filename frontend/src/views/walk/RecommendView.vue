d676447f5d05eb201638b37f425e29f5b586e504
New Name Value <mjmj9999@naver.com>
2024-02-11 22:48:19 +0900

    [FE] Fix : 추천 코스 가로 스크롤

--- frontend/src/views/walk/RecommendView.vue
+++ frontend/src/views/walk/RecommendView.vue
@@ -2,40 +2,44 @@
   <WalkHeaderNav />
   <div class="record-list-container">
     <div>
-      <h4>같은 동네 사람들은 이 경로로 산책했어요!</h4>
-      <div v-for="record in recordsTown" :key="record.recordSeq" :class="{ 'record-list': true }">
-        <div v-if="!record.notRecommended" class="recommend-list-item">
-          <shareBoardRecord
-            :duration="record.duration"
-            :distance="record.distance"
-            :points="record.points"
-            :title="record.title"
-            :seq="record.recordSeq"
-            :movable="false"
-            @click="moveDetail(record.recordSeq)"
-          />
-          <div class="action-buttons">
-            <p @click="notRecommend(record.recordSeq)">더 이상 이 코스 추천받지 않기</p>
+      <h2>같은 동네 사람들은 이 경로로 산책했어요!</h2>
+      <div class="recom-scroll-container">
+        <div v-for="record in recordsTown" :key="record.recordSeq" :class="{ 'record-list': true }">
+          <div v-if="!record.notRecommended" class="recommend-list-item">
+            <shareBoardRecord
+              :duration="record.duration"
+              :distance="record.distance"
+              :points="record.points"
+              :title="record.title"
+              :seq="record.recordSeq"
+              :movable="false"
+              @click="moveDetail(record.recordSeq)"
+            />
+            <div class="action-buttons">
+              <p @click="notRecommend(record.recordSeq)">더 이상 이 코스 추천받지 않기</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
     <div>
-      <h4>나와 비슷한 사람들은 이 경로로 산책했어요!</h4>
-      <div v-for="record in recordsInfo" :key="record.recordSeq" :class="{ 'record-list': true }">
-        <div v-if="!record.notRecommended" class="recommend-list-item">
-          <shareBoardRecord
-            :duration="record.duration"
-            :distance="record.distance"
-            :points="record.points"
-            :title="record.title"
-            :seq="record.recordSeq"
-            :movable="false"
-            @click="moveDetail(record.recordSeq)"
-          />
-          <div class="action-buttons">
-            <p @click="notRecommend(record.recordSeq)">더 이상 이 코스 추천받지 않기</p>
+      <h2>나와 비슷한 사람들은 이 경로로 산책했어요!</h2>
+      <div class="recom-scroll-container">
+        <div v-for="record in recordsInfo" :key="record.recordSeq" :class="{ 'record-list': true }">
+          <div v-if="!record.notRecommended" class="recommend-list-item">
+            <shareBoardRecord
+              :duration="record.duration"
+              :distance="record.distance"
+              :points="record.points"
+              :title="record.title"
+              :seq="record.recordSeq"
+              :movable="false"
+              @click="moveDetail(record.recordSeq)"
+            />
+            <div class="action-buttons">
+              <p @click="notRecommend(record.recordSeq)">더 이상 이 코스 추천받지 않기</p>
+            </div>
           </div>
         </div>
       </div>
@@ -89,5 +93,35 @@ const moveDetail = (seq) => {
 
 .action-buttons {
   margin-left: auto;
+}
+
+.record-list-container h2 {
+  color: white;
+  /* 제목 색상을 테마에 맞춥니다 */
+  font-size: 1.5rem;
+  /* 제목 글꼴 크기를 조정합니다 */
+  padding: 0.5rem 0;
+  /* 상하 패딩을 추가합니다 */
+  margin: 0;
+  /* 기본 마진을 제거합니다 */
+  text-align: center;
+  /* 제목을 중앙 정렬합니다 */
+  background-color: darkgreen;
+  /* 제목 배경 색상을 추가합니다 */
+  border-radius: 10px;
+  /* 배경의 모서리를 둥글게 합니다 */
+  box-shadow:
+    0 4px 6px rgba(50, 50, 93, 0.11),
+    0 1px 3px rgba(0, 0, 0, 0.08);
+  /* 그림자를 추가합니다 */
+}
+
+.recom-scroll-container {
+  display: flex;
+  flex-direction: row;
+  overflow-x: auto;
+  gap: 10px;
+  padding: 1rem 0;
+  white-space: nowrap;
 }
 </style>
